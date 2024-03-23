78a9face2fb927c57389259fa6c8978c32ac1977
Mohammad Abdur Rakib <77808270+marakib991@users.noreply.github.com>
2024-03-23 10:30:29 -0400

    Update config.js to apply dotenv

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5500;
+export const PORT = process.env.PORT;
 
 export const mongoDBURL = 'mongodb+srv://rarefigure:12345!aA@book-store.yzgdyox.mongodb.net/data1?retryWrites=true&w=majority'
